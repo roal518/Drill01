c627b86d0a7e33ab4899f10962fb5e50cb8a6028
roal518 <roalqwerty@gmail.com>
2023-09-14 11:39:07 +0900

    setheading 방향찾기 1
    왼쪽부터 찾아볼 계획

---
+++ turtle_move_keyboard.py
@@ -0,0 +1,11 @@
+import turtle
+
+def move_left():
+    turtle.setheading(90)
+    turtle.forward(50)
+    turtle.stamp()
+def restart():
+    turtle.reset()
+turtle.shape('turtle')
+turtle.onkey(move_left,'d')
+turtle.onkey(restart,'Escape')
